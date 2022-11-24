d7ea49c176107f12bfd61c1339055de2a7416e55
Vincent Le Jeune <vinzlj@gmail.com>
2022-11-24 16:45:35 +0100

    fix(performance): create a new wishlist only when adding a product

--- src/Controller/Action/AddProductToWishlistAction.php
+++ src/Controller/Action/AddProductToWishlistAction.php
@@ -10,39 +10,42 @@ declare(strict_types=1);
 
 namespace BitBag\SyliusWishlistPlugin\Controller\Action;
 
+use BitBag\SyliusWishlistPlugin\Entity\Wishlist;
 use BitBag\SyliusWishlistPlugin\Entity\WishlistInterface;
 use BitBag\SyliusWishlistPlugin\Entity\WishlistProductInterface;
 use BitBag\SyliusWishlistPlugin\Exception\WishlistNotFoundException;
+use BitBag\SyliusWishlistPlugin\Factory\WishlistFactoryInterface;
 use BitBag\SyliusWishlistPlugin\Factory\WishlistProductFactoryInterface;
 use BitBag\SyliusWishlistPlugin\Resolver\WishlistsResolverInterface;
 use Doctrine\Persistence\ObjectManager;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Sylius\Component\Channel\Context\ChannelNotFoundException;
+use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ShopUserInterface;
 use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class AddProductToWishlistAction
 {
     private ProductRepositoryInterface $productRepository;
-
     private WishlistProductFactoryInterface $wishlistProductFactory;
-
     private RequestStack $requestStack;
-
     private TranslatorInterface $translator;
-
     private WishlistsResolverInterface $wishlistsResolver;
-
     private ObjectManager $wishlistManager;
-
     private ChannelContextInterface $channelContext;
+    private WishlistFactoryInterface $wishlistFactory;
+    private Security $security;
+    private string $wishlistCookieTokenName;
 
     public function __construct(
         ProductRepositoryInterface $productRepository,
@@ -51,7 +54,10 @@ final class AddProductToWishlistAction
         TranslatorInterface $translator,
         WishlistsResolverInterface $wishlistsResolver,
         ObjectManager $wishlistManager,
-        ChannelContextInterface $channelContext
+        ChannelContextInterface $channelContext,
+        WishlistFactoryInterface $wishlistFactory,
+        Security $security,
+        string $wishlistCookieTokenName,
     ) {
         $this->productRepository = $productRepository;
         $this->wishlistProductFactory = $wishlistProductFactory;
@@ -60,6 +66,9 @@ final class AddProductToWishlistAction
         $this->wishlistsResolver = $wishlistsResolver;
         $this->wishlistManager = $wishlistManager;
         $this->channelContext = $channelContext;
+        $this->wishlistFactory = $wishlistFactory;
+        $this->security = $security;
+        $this->wishlistCookieTokenName = $wishlistCookieTokenName;
     }
 
     public function __invoke(Request $request): Response
@@ -71,21 +80,30 @@ final class AddProductToWishlistAction
             throw new NotFoundHttpException();
         }
 
+        try {
+            /** @var ChannelInterface $channel */
+            $channel = $this->channelContext->getChannel();
+        } catch (ChannelNotFoundException $exception) {
+            $channel = null;
+        }
+
         $wishlists = $this->wishlistsResolver->resolve();
 
         /** @var WishlistInterface $wishlist */
         $wishlist = array_shift($wishlists);
 
         if (null === $wishlist) {
-            throw new WishlistNotFoundException(
-                $this->translator->trans('bitbag_sylius_wishlist_plugin.ui.wishlist_not_found')
-            );
-        }
+            $user = $this->security->getUser();
 
-        try {
-            $channel = $this->channelContext->getChannel();
-        } catch (ChannelNotFoundException $exception) {
-            $channel = null;
+            if ($user instanceof ShopUserInterface) {
+                $wishlist = $this->wishlistFactory->createForUserAndChannel($user, $channel);
+            } else {
+                /** @var Wishlist $wishlist */
+                $wishlist = $this->wishlistFactory->createNew();
+                $wishlist->setChannel($channel);
+            }
+
+            $wishlist->setName('Wishlist');
         }
 
         if (null !== $channel && $wishlist->getChannel()->getId() !== $channel->getId()) {
@@ -99,6 +117,7 @@ final class AddProductToWishlistAction
 
         $wishlist->addWishlistProduct($wishlistProduct);
 
+        $this->wishlistManager->persist($wishlist);
         $this->wishlistManager->flush();
 
         /** @var Session $session */
@@ -109,6 +128,14 @@ final class AddProductToWishlistAction
         $referer = $request->headers->get('referer');
         $refererPathInfo = Request::create($referer)->getPathInfo();
 
-        return new RedirectResponse($refererPathInfo);
+        $response = new RedirectResponse($refererPathInfo);
+
+        if (!$request->cookies->has($this->wishlistCookieTokenName)) {
+            $cookie = new Cookie($this->wishlistCookieTokenName, $wishlist->getToken(), strtotime('+1 year'));
+
+            $response->headers->setCookie($cookie);
+        }
+
+        return $response;
     }
 }
